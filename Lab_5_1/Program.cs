52fafcbc220aebe23bb66902df9dbe94a9c2e20b
Никита Хромов <chromovnick@gmail.com>
2021-11-25 23:02:47 +0300

    Добавьте файлы проекта.

---
+++ Lab_5_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите элементы одномерного массива N= 7,1");
+            int N = 7;
+            int[] array = new int[7];
+            float average = 0;
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (float a in array)
+            {
+                average += (a / N);
+            }
+            Console.WriteLine("Cреднее арифметическое равняется {0:f2}", average);
+            Console.ReadKey();
+        }
+
+   
+    }
+}
---
+++ Lab_5_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целочисленное число N для получения массива NxN");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = 1 - (i + j) % 2;
+                    Console.Write("{0,6} ", array[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
